353ee1da99608079f32bdab447d3ff9f4b63271e
wondywang <superwondy@hotmail.com>
2018-11-15 15:25:41 +0800

    客户端收包异常逻辑处理。

--- apps/lookup_client/client.go
+++ apps/lookup_client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"net"
 	"os"
 	"log"
@@ -25,26 +26,30 @@ func main() {
 
 	logger.Debug("connecting sucess. %s", conn.RemoteAddr().String())
 	HeartBeat(conn)
+	logger.Warning("client disconnect. %s", conn.RemoteAddr().String())
 }
 
 func HeartBeat(conn net.Conn) {
-	done := make(chan string)
+	done := make(chan int)
 	ticker := time.NewTicker(3 * time.Second)
 	defer ticker.Stop()
 
 	for {
 		select {
-			case <- ticker.C:
-			{
-				logger.Debug("client heart beat.")
-				go handleWrite(conn, done)
-				go handleRead(conn, done)
-			}
+		case <- ticker.C:
+		{
+			logger.Debug("client heart beat.")
+			go handleWrite(conn, done)
+			go handleRead(conn, done)
+		}
+		case <- done:
+			logger.Warning("conn close. exit!")
+			return
 		}
 	}
 }
 
-func handleWrite(conn net.Conn, done chan string) {
+func handleWrite(conn net.Conn, done chan int) {
 	body := "hello"
 	buf := []byte("H")
 	bodySize := util.Int64ToBytes(int64(len(body)))
@@ -57,19 +62,27 @@ func handleWrite(conn net.Conn, done chan string) {
 	buf = append(buf, body...)
 	_, err := conn.Write(buf)
 	if err != nil {
+		if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+			logger.Debug("write message timeout. %s", err.Error())
+			return
+		}
+		done <- 1
 		logger.Error("Error to send message. %s", err.Error())
 		return
 	}
-	done <- "Send"
 }
 
-func handleRead(conn net.Conn, done chan string) {
+func handleRead(conn net.Conn, done chan int) {
 	buf := make([]byte, 1024)
 	len, err := conn.Read(buf)
 	if err != nil {
+		if err == io.EOF {
+			logger.Debug("read null message. %s", err.Error())
+			return
+		}
+		done <- 1
 		logger.Error("Error to read message. %s", err.Error())
 		return
 	}
 	logger.Debug(string(buf[:len-1]))
-	done <- "Receive"
 }
