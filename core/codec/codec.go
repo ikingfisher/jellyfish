163ba2aec705e025f731c27875688876170d189c
wondywang <superwondy@hotmail.com>
2018-11-20 00:49:59 +0800

    add header

--- core/codec/codec.go
+++ core/codec/codec.go
@@ -3,7 +3,14 @@ package codec
 import (
 	"bytes"
 	"encoding/gob"
+	"unsafe"
 )
+
+type Header struct {
+	T byte
+	Seq uint64
+	Size int32
+}
 
 type Request struct {
 	Cmd  string
@@ -13,6 +20,48 @@ type Request struct {
 type Response struct {
 	Cmd  string
 	Body []byte
+}
+
+func Encode(seq uint64, obj interface{}) ([]byte, error) {
+	var buff bytes.Buffer
+
+	var obj_buff bytes.Buffer
+	enc := gob.NewEncoder(&obj_buff)
+	err := enc.Encode(obj)
+	if err != nil {
+		return nil, err
+	}
+
+	var header_buff bytes.Buffer
+	var header Header
+	header.T = 'D'
+	header.Seq = seq
+	header.Size = len(obj_buff.Bytes())
+	header_enc := gob.NewEncoder(&header_buff)
+	err = header_enc.Encode(header)
+	if err != nil {
+		return nil, err
+	}
+
+	buff.Write(header_buff.Bytes())
+	buff.Write(obj_buff.Bytes())
+	buff.Write([]byte("E"))
+
+	return buff.Bytes(), nil
+}
+
+func HeaderSize() int {
+	return unsafe.Sizeof(Header{})
+}
+
+func Decode(body []byte, obj interface{}) error {
+	buff := bytes.NewBuffer(body)
+	dec := gob.NewDecoder(buff)
+	err := dec.Decode(obj)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func ReqEncode(req Request) ([]byte, error) {
